ec807a12ecefaeb8b76945f597b361eb4b851245
Nhan <nhan.t.ngo@outlook.com>
2022-12-12 16:09:31 -0800

    =add authenticate token/ register/ login route

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//Connect Database
+connectDB()
+
+//Init middleware
+app.use(express.json({extented: false}))
+
+app.get('', (req, res) => res.send('API is running'))
+app.use('/api/user', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server is on  port ${PORT}`))
-app.get('', (req, res) => res.send('API is running') )
